38ab271ca10ba9ede691711eb2b5fd1e407967ca
magiciangambit <khairulariffin@gmx.com>
2018-11-21 15:14:17 +0800

    Global Styling for Xamarin

---
+++ UsingGlobalStyles/UsingGlobalStyles/Styling/GlobalResourceDictionary.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace UsingGlobalStyles
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class GlobalResourceDictionary : ResourceDictionary
+    {
+		public GlobalResourceDictionary ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
